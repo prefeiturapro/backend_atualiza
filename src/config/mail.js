4db8a50abe14a7ca359350fe333fbe7501e268ee
Alex Raulino <prefeiturapro@gmail.com>
2026-03-16 15:40:40 -0300

    novaaa

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,19 +1,23 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  service: 'gmail', // Deixe o Nodemailer gerenciar os detalhes
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  // ADICIONE ESTE BLOCO ABAIXO:
+  pool: true, // Usa conexões persistentes
+  maxConnections: 1,
+  maxMessages: Infinity,
+  connectionTimeout: 10000, // 10 segundos
 });
 
-// Isso é fundamental para debug no Render
 transporter.verify((error, success) => {
   if (error) {
     console.error("[ERRO NO MAIL.JS]:", error.message);
   } else {
-    console.log("[SUCESSO]: Servidor de e-mail pronto para decolar!");
+    console.log("✅ Conexão com o Gmail estabelecida!");
   }
 });
 
