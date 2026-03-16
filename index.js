e6e25bbe7700a48830a00ba1b15ab2089b059171
Alex Raulino <prefeiturapro@gmail.com>
2026-03-16 14:24:33 -0300

    atualizacao

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const app = express();
 app.use(express.json({ limit: '50mb' })); 
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors({
-    origin: '*' 
-}));
-
+app.use(cors()); // Libera tudo de forma simples
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+  next();
+});
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // === IMPORTAÇÃO DOS CONTROLLERS / ROTAS ===
